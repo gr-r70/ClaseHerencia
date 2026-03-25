d4927c84b1a9386af01086b9ced1aad471eb7313
gr-r70 <german_ren.rojas@uao.edu.co>
2026-03-25 10:51:20 -0500

    Arreglo en el paquete modelo

---
+++ src/main/java/Modelo/Empleado.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+public class Empleado {
+    protected String nombre;
+    protected double salarioBase;
+    
+    public Empleado (String nombre, double salarioBase){
+        this.nombre = nombre;
+        this.salarioBase= salarioBase;
+    }
+    
+    public double calcularPago(){
+        return salarioBase;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    
+    
+}
