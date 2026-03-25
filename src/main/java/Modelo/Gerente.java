e6f1eff8e37ce4c0eea2b6fb8975af91175bc128
gr-r70 <german_ren.rojas@uao.edu.co>
2026-03-25 11:28:28 -0500

    Arreglo final del programa

---
+++ src/main/java/Modelo/Gerente.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Gerente extends Empleado{
+    private double bono;
+    
+    public Gerente (String nombre, double salarioBase, double bono){
+        super(nombre,salarioBase);
+        this.bono = bono;
+    }
+    
+    @Override // Cunado se va a sobre escribir algo
+    public double calcularPago(){
+        return salarioBase + bono;
+    }
+}
--- src/main/java/Vista/VistaHerencia.java
+++ src/main/java/Vista/VistaHerencia.java
@@ -4,10 +4,38 @@
  */
 package Vista;
 
-/**
- *
- * @author Usuario
- */
+import Controlador.NominaController;
+import Modelo.Gerente;
+import Modelo.Empleado;
 public class VistaHerencia {
+    public static void main (String[] args){
+        System.out.println("aprendiendo Herencia");
+        NominaController control = new NominaController ();
+        
+        control.registrarEmpleados(new Empleado("Danilo Eutanasia", 1000));
+        control.registrarEmpleados(new Gerente("Danilo Eutanasia2", 7000, 500));
+        
+        System.out.println("==Lista de Pagos==");
+        for (Empleado emp : control.obtenerLista()){
+            //aquí actual el polimorfismo : cada objeto sabe como calcular su pago
+            
+            System.out.println("Nombre: " + emp.getNombre());
+            System.out.println("Pago: "+ emp.calcularPago());
+            System.out.println("-------------------------");
+        
+            System.out.println("=prueba de sobre carga=");
+            Empleado busqueda1 =control.buscar("Danilo Eutanasia");
+            Empleado busqueda2 =control.buscar(1);
+            
+            System.out.println("Prueba de sobrecarga busqueda por nombre: "+ (busqueda1 !=null? busqueda1.getNombre():"No existe"));
+            System.out.println("Prueba de sobrecarga busqueda por índice: "+ (busqueda2 !=null? busqueda1.getNombre():"No existe"));
+        }
+        
+        
+        
+        
+    }
+
+    
     
 }
